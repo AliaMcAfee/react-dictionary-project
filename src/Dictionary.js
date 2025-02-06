61fa018cf0b5d32bb76323528e2c851383810cfe
AliaMcAfee <mcafeealia@gmail.com>
2025-02-06 16:48:16 -0600

    imported axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(` Searching for the ${word} definition.....`);
+
+    let apiKey = "04d7a4ta8ecdfa4b30ebd32f8c464o31";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateWord(event) {
