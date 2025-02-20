c331c4bf98d32e7c4beffa154d2082f7470041d1
AliaMcAfee <mcafeealia@gmail.com>
2025-02-20 10:32:53 -0600

    adding examples and synonyms

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 export default function Definitions(props) {
-  console.log(props.definition);
   if (props.definition) {
     return (
       <div className="Definitions">
-        <ul>
-          <li>Part of Speech: {props.definition.partOfSpeech}</li>
-          <li>Definition: {props.definition.definition}</li>
-          <li>Example: {props.definition.example}</li>
+        <ul className="results">
+          <li>
+            {" "}
+            <strong>Part of Speech:</strong> {props.definition.partOfSpeech}
+          </li>
+          <li>
+            <strong>Synonyms:</strong> {props.definition.synonyms}
+          </li>
+          <li>
+            {" "}
+            <strong>Definition:</strong> {props.definition.definition}
+          </li>
+          <li>
+            <strong>Example:</strong>
+            {props.definition.example}
+          </li>
         </ul>
       </div>
     );
