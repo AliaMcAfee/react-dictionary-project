a4eeecd43bb69232d5caf6d5b3930a7ea06ed96c
AliaMcAfee <mcafeealia@gmail.com>
2025-02-20 11:53:16 -0600

    Added synonyms

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Definitions(props) {
   if (props.definition) {
@@ -6,18 +7,18 @@ export default function Definitions(props) {
       <div className="Definitions">
         <ul className="results">
           <li>
-            {" "}
-            <strong>Part of Speech:</strong> {props.definition.partOfSpeech}
-          </li>
-          <li>
-            <strong>Synonyms:</strong> {props.definition.synonyms}
-          </li>
-          <li>
-            {" "}
             <strong>Definition:</strong> {props.definition.definition}
           </li>
           <li>
-            <strong>Example:</strong>
+            <strong>Part of Speech:</strong> {props.definition.partOfSpeech}
+          </li>
+          <li>
+            <strong>Synonyms:</strong>
+            <Synonyms synonyms={props.definition.synonyms} />
+          </li>
+
+          <li>
+            <strong>Example: </strong>
             {props.definition.example}
           </li>
         </ul>
